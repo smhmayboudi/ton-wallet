e994eb566940267007c67029238358db7a1f71dc
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-05-16 16:04:42 +0330

    send transfer transaction

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {getHttpEndpoint} from '@orbs-network/ton-access';
 import {mnemonicToWalletKey} from '@ton/crypto';
-import {WalletContractV4, TonClient, fromNano} from '@ton/ton';
+import {WalletContractV4, TonClient, fromNano, internal} from '@ton/ton';
 
 async function main() {
   // open wallet v4 (notice the correct wallet version here)
@@ -29,6 +29,38 @@ async function main() {
   const walletContract = client.open(wallet);
   const seqno = await walletContract.getSeqno();
   console.log('seqno:', seqno);
+
+  // make sure wallet is deployed
+  if (!(await client.isContractDeployed(wallet.address))) {
+    return console.log('wallet is not deployed');
+  }
+
+  // send 0.05 TON to EQA4V9tF4lY2S_J-sEQR7aUj9IwW-Ou2vJQlCn--2DLOLR5e
+  await walletContract.sendTransfer({
+    secretKey: key.secretKey,
+    seqno: seqno,
+    messages: [
+      internal({
+        to: 'EQA4V9tF4lY2S_J-sEQR7aUj9IwW-Ou2vJQlCn--2DLOLR5e',
+        value: '0.05', // 0.05 TON
+        body: 'Hello', // optional comment
+        bounce: false,
+      }),
+    ],
+  });
+
+  // wait until confirmed
+  let currentSeqno = seqno;
+  while (currentSeqno === seqno) {
+    console.log('waiting for transaction to confirm...');
+    await sleep(1500);
+    currentSeqno = await walletContract.getSeqno();
+  }
+  console.log('transaction confirmed!');
 }
 
 main();
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
