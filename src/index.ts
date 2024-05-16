2f8a83e574a3eeade178311a1c4bfdb3f84b8a84
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-05-16 15:51:18 +0330

    read wallet state

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import {getHttpEndpoint} from '@orbs-network/ton-access';
 import {mnemonicToWalletKey} from '@ton/crypto';
-import {WalletContractV4} from '@ton/ton';
+import {WalletContractV4, TonClient, fromNano} from '@ton/ton';
 
 async function main() {
   // open wallet v4 (notice the correct wallet version here)
@@ -15,6 +16,19 @@ async function main() {
 
   // print wallet workchain
   console.log('workchain:', wallet.address.workChain);
+
+  // initialize ton rpc client on testnet
+  const endpoint = await getHttpEndpoint({network: 'testnet'});
+  const client = new TonClient({endpoint});
+
+  // query balance from chain
+  const balance = await client.getBalance(wallet.address);
+  console.log('balance:', fromNano(balance));
+
+  // query seqno from chain
+  const walletContract = client.open(wallet);
+  const seqno = await walletContract.getSeqno();
+  console.log('seqno:', seqno);
 }
 
 main();
